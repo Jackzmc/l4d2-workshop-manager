5e2d72c147152b726c81a753623387fbdfb96ecc
Jackz <me@jackz.me>
2021-06-13 22:11:01 -0500

    Improve settings UI

--- src/components/sections/Settings.vue
+++ src/components/sections/Settings.vue
@@ -1,10 +1,25 @@
 <template>
 <div class="container">
     <div class="mt-2 box">
-        <p>
-            <b><u>Addons Folder</u></b> {{settings.gamedir}}
-            <a class="button is-small is-info is-inline is-pulled-right">Update</a>
-        </p>
+        <div class="field">
+            <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                    <label class="label">Addons Folder</label>
+                </div>
+                <div class="field-body">
+                    <div class="field has-addons has-addons-centered">
+                        <p class="control">
+                            <input class="input" style="width:600px" type="text" :value="settings.gamedir">
+                        </p>
+                        <p class="control">
+                            <a class="button is-info">
+                            Update
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 </template>
@@ -14,3 +29,9 @@ export default {
     props: ['settings']
 }
 </script>
+
+<style scoped>
+li {
+    margin-bottom: 1em;
+}
+</style>
